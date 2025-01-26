b8617d9264bb3b462f5b5f998641482b841268bc
Razvan Marescu <razvan@marescu.net>
2025-01-25 21:34:17 -0800

    Reproduction 401 API error

--- example.test.ts
+++ example.test.ts
@@ -1,5 +1,7 @@
 import { shortest } from "@antiwork/shortest";
 
 // Uses baseUrl from config to determine what page to visit.
-shortest("Perform a search for 'shortest'")
-  .expect("The search results include a GitHub 'shortest' repo by Anti-Work.");
+shortest("Click on `Show alert box`")
+  .expect("Alert is shown with text `I am an alert box!`")
+  .expect("Close the alert")
+  .expect("Text `You triggered and handled the alert dialog` is displayed below the alert button.");
--- shortest.config.ts
+++ shortest.config.ts
@@ -3,7 +3,7 @@ import type { ShortestConfig } from "@antiwork/shortest";
 
 export default {
   headless: false,
-  baseUrl: "https://github.com",
+  baseUrl: "https://testpages.herokuapp.com/styled/alerts/alert-test.html",
   testPattern: "**/*.test.ts",
   anthropicKey: process.env.ANTHROPIC_API_KEY,
 } satisfies ShortestConfig;
