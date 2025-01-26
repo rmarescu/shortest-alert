608473f5a2fae5b7746c475cfc07a42e9fe526de
Razvan Marescu <razvan@marescu.net>
2025-01-25 21:43:44 -0800

    Reproduction confirm

--- example.test.ts
+++ example.test.ts
@@ -1,7 +1,12 @@
 import { shortest } from "@antiwork/shortest";
 
 // Uses baseUrl from config to determine what page to visit.
-shortest("Click on `Show alert box`")
-  .expect("Alert is shown with text `I am an alert box!`")
-  .expect("Close the alert")
-  .expect("Text `You triggered and handled the alert dialog` is displayed below the alert button.");
+shortest("Click on `Show confirm box`")
+  .expect("Confirmation box is shown with text `I am a confirm alert`")
+  .expect("Click `OK`")
+  .expect("`You clicked OK, confirm returned true.` is displayed below the confirm button.");
+
+// shortest("Click on `Show prompt box`")
+//   .expect("Change the existing text of the prompt to `Hello World`")
+//   .expect("Click `OK`")
+//   .expect("`You clicked OK. 'prompt' returned Hello World` is displayed below the prompt button.");
